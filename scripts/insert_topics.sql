266ebf7bd50b6f62c544f810297888c5b531976a
Kyle <kmeel089@uottawa.ca>
2016-04-01 17:29:07 -0400

    Insert scripts added
    Movies and Topics now have some data.

---
+++ scripts/insert_topics.sql
@@ -0,0 +1,25 @@
+﻿set search_path = "project";
+
+insert into topics(topic_id, description) values(100,'Surreal');
+insert into topics(topic_id, description) values(101,'Action');
+insert into topics(topic_id, description) values(102,'Adventure');
+insert into topics(topic_id, description) values(103,'Comedy');
+insert into topics(topic_id, description) values(104,'Crime');
+insert into topics(topic_id, description) values(105,'Drama');
+insert into topics(topic_id, description) values(106,'Fantasy');
+insert into topics(topic_id, description) values(107,'Historical');
+insert into topics(topic_id, description) values(108,'Historical fiction');
+insert into topics(topic_id, description) values(109,'Horror');
+insert into topics(topic_id, description) values(111,'Mystery');
+insert into topics(topic_id, description) values(112,'Paranoid');
+insert into topics(topic_id, description) values(113,'Philosophical');
+insert into topics(topic_id, description) values(114,'Political');
+insert into topics(topic_id, description) values(115,'Romance');
+insert into topics(topic_id, description) values(116,'Saga');
+insert into topics(topic_id, description) values(117,'Satire');
+insert into topics(topic_id, description) values(118,'Science fiction');
+insert into topics(topic_id, description) values(119,'Slice of life');
+insert into topics(topic_id, description) values(121,'Animation');
+insert into topics(topic_id, description) values(122,'Thriller');
+insert into topics(topic_id, description) values(123,'Urban');
+insert into topics(topic_id, description) values(124,'Western');
