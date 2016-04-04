2ffa30845f8259d6a46525ae8ff61ae708f561e4
Kyle <kmeel089@uottawa.ca>
2016-04-04 14:28:25 -0400

    Director script added
    Some directors added to the script, not complete yet

---
+++ scripts/insert_directors.sql
@@ -0,0 +1,18 @@
+﻿set search_path = "project";
+
+insert into Director(director_id, first_name, last_name, country) values(100,'Frank','Darabont','France');
+insert into Director(director_id, first_name, last_name, country) values(101,'Francis','Coppola','USA');
+insert into Director(director_id, first_name, last_name, country) values(102,'Christopher','Nolan','England');
+insert into Director(director_id, first_name, last_name, country) values(103,'Steven','Spielberg','USA');
+insert into Director(director_id, first_name, last_name, country) values(104,'Sidney','Lumet','USA');
+insert into Director(director_id, first_name, last_name, country) values(105,'Quentin','Tarantino','USA');
+insert into Director(director_id, first_name, last_name, country) values(106,'Peter','Jackson','New Zealand');
+insert into Director(director_id, first_name, last_name, country) values(107,'Sergio','Leone','Italy');
+insert into Director(director_id, first_name, last_name, country) values(108,'David','Fincher','USA');
+insert into Director(director_id, first_name, last_name, country) values(109,'Irvin','Kershner','USA');
+insert into Director(director_id, first_name, last_name, country) values(110,'Robert','Zemeckis','USA');
+insert into Director(director_id, first_name, last_name, country) values(111,'Milos','Forman','Czeck Republic');
+insert into Director(director_id, first_name, last_name, country) values(112,'Martin','Scorsese','USA');
+insert into Director(director_id, first_name, last_name, country) values(113,'Lana','Wachowski','USA');
+insert into Director(director_id, first_name, last_name, country) values(114,'Lilly','Wachowski','USA');
+insert into Director(director_id, first_name, last_name, country) values(115,'Akira','Kurosawa','Japan');
