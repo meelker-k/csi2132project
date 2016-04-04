4ee8b7ee0e84d58b9067066ee40462ba3c938c1f
Kyle <kmeel089@uottawa.ca>
2016-04-04 15:02:48 -0400

    Updated director script

--- scripts/insert_directors.sql
+++ scripts/insert_directors.sql
@@ -2,7 +2,7 @@
 
 insert into Director(director_id, first_name, last_name, country) values(100,'Frank','Darabont','France');
 insert into Director(director_id, first_name, last_name, country) values(101,'Francis','Coppola','USA');
-insert into Director(director_id, first_name, last_name, country) values(102,'Christopher','Nolan','England');
+insert into Director(director_id, first_name, last_name, country) values(102,'Christopher','Nolan','UK');
 insert into Director(director_id, first_name, last_name, country) values(103,'Steven','Spielberg','USA');
 insert into Director(director_id, first_name, last_name, country) values(104,'Sidney','Lumet','USA');
 insert into Director(director_id, first_name, last_name, country) values(105,'Quentin','Tarantino','USA');
@@ -16,3 +16,18 @@ insert into Director(director_id, first_name, last_name, country) values(112,'Ma
 insert into Director(director_id, first_name, last_name, country) values(113,'Lana','Wachowski','USA');
 insert into Director(director_id, first_name, last_name, country) values(114,'Lilly','Wachowski','USA');
 insert into Director(director_id, first_name, last_name, country) values(115,'Akira','Kurosawa','Japan');
+insert into Director(director_id, first_name, last_name, country) values(116,'George','Lucas','USA');
+insert into Director(director_id, first_name, last_name, country) values(117,'Fernando','Meirelles','Brazil');
+insert into Director(director_id, first_name, last_name, country) values(118,'Katia','Lund','Brazil');
+insert into Director(director_id, first_name, last_name, country) values(119,'Jonathan','Demme','USA');
+insert into Director(director_id, first_name, last_name, country) values(120,'Frank','Capra','Italy');
+insert into Director(director_id, first_name, last_name, country) values(121,'Bryan','Singer','USA');
+insert into Director(director_id, first_name, last_name, country) values(122,'Roberto','Benigni','Italy');
+insert into Director(director_id, first_name, last_name, country) values(123,'Luc','Besson','France');
+insert into Director(director_id, first_name, last_name, country) values(124,'Hayao','Miyazaki','Japan');
+insert into Director(director_id, first_name, last_name, country) values(125,'Michael','Curtiz','USA');
+insert into Director(director_id, first_name, last_name, country) values(126,'Tony','Kaye','UK');
+insert into Director(director_id, first_name, last_name, country) values(127,'Charles','Chaplin','UK');
+insert into Director(director_id, first_name, last_name, country) values(128,'Alfred','Hitchcock','UK');
+insert into Director(director_id, first_name, last_name, country) values(129,'Olivier','Nakache','France');
+insert into Director(director_id, first_name, last_name, country) values(130,'Eric','Toledano','France');
